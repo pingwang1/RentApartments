ca4d33df5b6bb6a7eb60f1228cb6ab55620496c8
pingwang1 <1274059909@qq.com>
2017-12-29 16:08:00 +0800

    start project frame

---
+++ constants.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+
+PIC_CODE_EXPIRES_SECONDS = 180 #图片验证码的有效期,单位秒
+SMS_CODE_EXPIRES_SECONDS = 300 #短信验证码的有效期,单位秒
+
+SESSION_EXPIRES_SECONDS = 86400 #session数据有效期,单位秒
+
+QINIU_URL_PREFIX = 'http://o91qujnqh.bkt.clouddn.com/'
+
+REDIS_AREA_INFO_EXPIRES_SECONDES = 86400#redis缓存城区信息的有效期
+REDIS_HOUSE_INFO_EXPIRE_SECONDS = 86400 #redis缓存房屋信息的有效时间
+
+HOME_PAGE_MAX_HOUSES = 5 #主页房屋展示的最大数目
+HOME_PAGE_DATA_REDIS_EXPIRE_SECONDS= 7200 #主页缓存数据过期时间 秒
+
+HOUSE_LIST_PAGE_CAPACITY =3 #房源列表每页显示房屋的数目
+HOUSE_LIST_PAGE_CACHE_NUM = 2 #房源列表每次缓存页面数
+
+REDIS_HOUSE_LIST_EXPIRES_SECONDS = 7200 #列表页数据缓存时间 秒
---
+++ utils/qiniu_storage.py
@@ -0,0 +1,43 @@
+#coding:utf-8
+
+import qiniu.config
+import logging
+
+from qiniu import Auth,put_data,etag,urlsafe_base64_encode
+
+#需要填写你的Access Key 和Secret Key
+access_key = 'uzc59bVURbUbazey9vrexXKocNKBUN8NuLijk57N'
+secret_key = '-9lenw28jU2REojvGkcsEPWk5Nm9V2HIVqb5Nkts'
+
+def storage(file_data):
+	try:
+		#构建鉴权对象
+		q=Auth(access_key,secret_key)
+
+		#要上传的空间
+		bucket_name = 'ihome'
+		#生成上传Token,可以指定过期时间
+		token = q.upload_token(bucket_name)
+
+		ret,info = put_data(token,None,file_data)
+
+	except Exception as e:
+		logging.error(e)
+		raise e
+	print(ret)
+	print("*"*16)
+	print(info)
+
+	print(type(info))
+	print(info.status_code)
+	if 200 == info.status_code:
+		return ret['key']
+	else:
+		raise Exception("上传失败")
+
+
+if __name__ == "__main__":
+	file_name = raw_input("input file name")
+	with open(file_name,'rb') as file:
+		file_data.=file.read()
+		storage(file_data)
